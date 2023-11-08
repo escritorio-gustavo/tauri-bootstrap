419af05c95e09967e3c01b38f1e8b42b4af7b07f
Gustavo <gustavo.shigueo@proton.me>
2023-11-08 18:44:02 -0300

    Update config files

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxSingleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+}
