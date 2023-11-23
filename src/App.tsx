1ba6b1ee02a92d76dd034e00340fdc28856af9b0
Gustavo <gustavo.shigueo@proton.me>
2023-11-23 15:47:12 -0300

    Add UI template

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,61 @@
-import { createSignal } from 'solid-js'
-import { invoke } from '@tauri-apps/api/tauri'
+import {
+  Button,
+  PaginatedList,
+  PaginationControls,
+  ProgressBar,
+  Table,
+} from 'shelf-ui'
+import { Show } from 'solid-js'
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal('')
-  const [name, setName] = createSignal('')
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke('greet', { name: name() }))
-  }
-
   return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
+    <div class="grid h-screen w-screen grid-cols-[minmax(40ch,_1fr)_3fr] gap-2 overflow-hidden">
+      <section class="flex h-full flex-col gap-4 overflow-auto bg-neutral-200 p-4 dark:bg-neutral-900">
+        <h1 class="text-xl font-bold">{'{{app_name}}'}</h1>
 
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <form
-        class="row"
-        onSubmit={e => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={e => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+        <PaginatedList
+          class="divider-neutral-600 mb-auto min-h-[10em] flex-grow divide-y-2 overflow-auto rounded bg-neutral-300 p-2 dark:bg-neutral-700"
+          pageSize={100}
+          items={[]}
+          render={x => x}
         />
-        <button type="submit">Greet</button>
-      </form>
 
-      <p>{greetMsg()}</p>
+        <Show when={true}>
+          <ProgressBar displayMax={0} displayValue={0} value={0} max={0} />
+        </Show>
+
+        <Button disabled={false} onClick={() => undefined}>
+          Importar arquivo
+        </Button>
+
+        <Button variant="success" disabled={true} onClick={() => undefined}>
+          Pesquisar
+        </Button>
+      </section>
+
+      <section class="flex-col items-center overflow-auto em:gap-2">
+        <PaginationControls
+          page={0}
+          onPageChange={async () => undefined}
+          numberOfPages={1}
+        />
+
+        <Table
+          class="[&_td]:align-baseline [&_td_>_div]:line-clamp-5 [&_td_>_p]:w-max [&_td_>_p]:max-w-[75ch]"
+          columns={[]}
+          data={[]}
+        />
+      </section>
+
+      <Show when={false}>
+        <Button
+          onClick={() => undefined}
+          variant="primary"
+          class="fixed bottom-5 right-5"
+        >
+          Exportar
+        </Button>
+      </Show>
     </div>
   )
 }
