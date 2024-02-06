90e4b6312a4682e6cc885c54d8c59fb5660380bf
Gustavo <gustavo.shigueo@proton.me>
2024-02-06 17:02:10 -0300

    Download browser on launch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,17 +14,21 @@ pub mod error;
 pub mod prelude;
 pub mod scraper;
 
+pub static EXECUTABLE_PATH: OnceCell<PathBuf> = OnceCell::const_new();
 lazy_static! {
+    pub static ref TAURI_CONFIG: tauri::Config =
+        serde_json::from_str(include_str!("../tauri.conf.json")).unwrap();
+    pub static ref BROWSER_PATH: PathBuf = tauri::api::path::app_local_data_dir(&TAURI_CONFIG)
+        .unwrap()
+        .join("browser");
     pub static ref DATA_SET: RwLock<Vec<Arc<()>>> = RwLock::new(vec![]);
 }
 
-pub struct BrowserManagerState {
-    browser_manager_mutex: Arc<Mutex<BrowserManager>>,
-}
-
+#[tokio::main]
 fn main() {
     let browser_manager_state = BrowserManagerState::new();
 
+    download_browser().await;
 
     tauri::Builder::default()
         .manage(browser_manager_state)
@@ -39,4 +43,24 @@ fn main() {
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn download_browser() {
+    let _ = std::fs::create_dir_all(BROWSER_PATH.as_path());
+    let fetcher = chromiumoxide::BrowserFetcherOptions::builder()
+        .with_path(BROWSER_PATH.as_path())
+        .build()
+        .map(chromiumoxide::BrowserFetcher::new)
+        .expect("Path must be provided to build BrowserFetcherOptions");
+
+    match fetcher.fetch().await {
+        Ok(info) => {
+            EXECUTABLE_PATH
+                .set(info.executable_path)
+                .expect("This should only happen once");
+        }
+        Err(e) => {
+            eprintln!("Fetcher failed: {e}");
+        }
+    };
 }
--- src-tauri/src/scraper/browser_factory.rs
+++ src-tauri/src/scraper/browser_factory.rs
@@ -1,30 +1,18 @@
+use chromiumoxide::{Browser, BrowserConfig, Handler};
 use rand::RngCore;
-use std::path::Path;
 
-use chromiumoxide::{Browser, BrowserConfig, BrowserFetcher, BrowserFetcherOptions, Handler};
-
-use crate::prelude::*;
+use crate::{prelude::*, BROWSER_PATH, EXECUTABLE_PATH};
 
 pub(super) async fn create_browser() -> Result<(Browser, Handler)> {
-    let download_path = Path::new(r".\download");
-    let _ = std::fs::create_dir_all(download_path);
-
-    let fetcher_config = BrowserFetcherOptions::builder()
-        .with_path(download_path)
-        .build()?;
-
-    let fetcher = BrowserFetcher::new(fetcher_config);
-
-    let info = fetcher.fetch().await?;
-
     let number = rand::thread_rng().next_u64();
     let config_builder = BrowserConfig::builder()
-        .chrome_executable(info.executable_path)
+        .chrome_executable(EXECUTABLE_PATH.get().unwrap())
         .launch_timeout(std::time::Duration::from_secs(20))
         .no_sandbox()
         .request_timeout(std::time::Duration::from_secs(3_600))
         .enable_request_intercept()
-        .user_data_dir(format!(r"..\..\data-dir-{number}"));
+        .user_data_dir(BROWSER_PATH.join(format!(r"data-dir-{number}")));
+
     #[cfg(dev)]
     let config_builder = config_builder.with_head();
 
